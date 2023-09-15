972fa81ab71bb829d8e74c1f604e1adc27d783de
Aurélien Porté <aurel.porte@gmail.com>
2023-09-15 18:06:13 +0200

    hmtl : intégration modale ajout photo. CSS : style formulaire aajout photo. JS: validation des inputs

--- FrontEnd/script_front.js
+++ FrontEnd/script_front.js
@@ -117,12 +117,13 @@ if(localStorage.getItem("token")){
 //Generation dynamique des images de la modale
 const modale = document.querySelector("dialog");
 const conteneurImagesModale = document.querySelector(".conteneur-images");
-
+//Apelle la fonction creerItemModale autant de fois que la variable contient d'element, ouvre la modale ensuite
 function chargerGalerieModale (){
     conteneurImagesModale.innerHTML="";
     works.forEach(element => creerItemModale(element));
     modale.showModal();
 }
+//Creer une miniature avec icone poubelle, ajouter style et inserer element dans DOM
 function creerItemModale (element){
     const divImgPlusIcone = document.createElement("div");
     divImgPlusIcone.classList.add("conteneur-img-icone");
@@ -132,18 +133,181 @@ function creerItemModale (element){
     const iconePoubelle = document.createElement("i");
     iconePoubelle.classList.add("fa-solid");
     iconePoubelle.classList.add("fa-trash-can");
+    iconePoubelle.setAttribute("id",element.id);
+    iconePoubelle.addEventListener("click", supprimer);
     divImgPlusIcone.append(image);
     divImgPlusIcone.append(iconePoubelle);
     conteneurImagesModale.append(divImgPlusIcone);
 }
 
-//Fermer la modale au clic du bouton "x" ou clic en dehors de la boite
-const boutonFermerModale = document.querySelector(".bouton-fermer-modale");
-boutonFermerModale.addEventListener("click", () => {
+//Fermer la modale au clic des bouton "x"
+const boutonFermerModale = document.querySelectorAll(".bouton-fermer-modale");
+boutonFermerModale[0].addEventListener("click", ()=>{
     modale.close();
 });
-modale.addEventListener("click", (e) => {
-    if(e.offsetX > modale.offsetWidth || e.offsetX < 0 || e.offsetY > modale.offsetHeight || e.offsetY < 0 ){
-        modale.close();
-    }
+boutonFermerModale[1].addEventListener("click", ()=>{
+    modaleAjout.close();
 });
+
+//Fermer la modale en cliquant à l'extérieur de la boite
+modale.addEventListener("click", fermerClicExterieur);
+function fermerClicExterieur (e){
+    if(e.offsetX > this.offsetWidth || e.offsetX < 0 || e.offsetY > this.offsetHeight || e.offsetY < 0 ){
+        this.close();
+    }
+}
+//Supprimer un élément
+function supprimer (e){
+    const id = e.target.getAttribute("id");
+    const token = localStorage.getItem("token");
+    fetch("http://localhost:5678/api/works/"+id, {
+        method:"DELETE",
+        headers: {
+            "accept": "*/*",
+            "authorization": "Bearer"+" "+token},
+    });
+}
+//Ouvrir la modale pour ajouter un élément depuis le bouton "ajouter une photo"
+const boutonAjouter = document.querySelector(".js-ajout");
+boutonAjouter.addEventListener("click", ajouterUnePhoto);
+//Ferme la modale galerie, ouvre la modale ajout, formulaire reinitialisé
+function ajouterUnePhoto (){
+    modale.close();
+    effacerFormulaire();
+    creerCategories();
+    modaleAjout.showModal();
+}
+const modaleAjout = document.getElementById("modale-ajout");
+modaleAjout.addEventListener("click", fermerClicExterieur);
+const retourModale = document.querySelector(".nav-modale a[href=\"#modale-galerie\"]");
+retourModale.addEventListener("click", ()=>{
+    modaleAjout.close();
+    modale.showModal();
+})
+//Revient à un formaulaire vide
+function effacerFormulaire (){
+    apercu.remove();
+    iconeFaImage.classList.remove("invisible");
+    labelImage.classList.remove("invisible");
+    boutonAjouterPhoto.classList.remove("invisible");
+    inputTitre.value="";
+    inputTitre.classList.remove("invalide");
+    messageErreur.innerHTML="";
+    messageErreur.classList.remove("erreur");
+    selecteurCategorie.classList.remove("invalide");
+    erreurCategorie.innerHTML="";
+    erreurCategorie.classList.remove("erreur");
+}
+//Creer dynamiquement la liste d'options "catégorie" du formulaire d'ajout
+const selecteurCategorie = document.querySelector("select");
+function creerCategories (){
+    let listeOptions = categories.slice(1);
+    selecteurCategorie.innerHTML="";
+    const optionVide = document.createElement("option");
+    optionVide.setAttribute("value",null);
+    selecteurCategorie.append(optionVide);
+    for(choix of listeOptions){
+        const option = document.createElement("option");
+        option.innerHTML = choix;
+        option.setAttribute("value",choix);
+        selecteurCategorie.append(option);
+    }
+}
+//Faire apparaitre une image miniature après la sélection d'un fichier
+const conteneurUpload = document.querySelector(".conteneur-upload");
+const iconeFaImage = document.querySelector(".fa-image");
+const inputImage = document.querySelector("input[name=\"image\"]");
+const boutonAjouterPhoto = document.querySelector(".bouton-ajouter");
+const labelImage = document.querySelector("label[for=\"image\"]");
+const apercu = document.createElement("img");
+
+//Validation et affichage du fichier selectionné
+inputImage.addEventListener("change", ()=>{
+    const lecteurImage = new FileReader();
+    lecteurImage.addEventListener("load", (e)=>{
+        apercu.classList.add("apercu");
+        apercu.src=e.target.result;
+        conteneurUpload.prepend(apercu);
+        iconeFaImage.classList.add("invisible");
+        labelImage.classList.add("invisible");
+        boutonAjouterPhoto.classList.add("invisible");
+    });
+    lecteurImage.readAsDataURL(inputImage.files[0]);
+});
+
+//Validation du champ de saisie pour le titre
+const inputTitre = document.querySelector("#titre");
+inputTitre.addEventListener("change", validerTitre);
+const messageErreur = document.querySelector("label[for=titre] span");
+
+function validerTitre (){
+    const titre = inputTitre.value;
+    const regExTitre = /\w{2,}/;
+    if(regExTitre.test(titre)=== false){
+        inputTitre.classList.add("invalide");
+        messageErreur.classList.add("erreur");
+        messageErreur.innerHTML="Titre invalide, 2 lettres min";
+    }
+    else{
+        inputTitre.classList.remove("invalide");
+        messageErreur.innerHTML="";
+        return true
+    }
+}
+//Validation du choix d'une option dans la liste
+selecteurCategorie.addEventListener("change", validerCategorie);
+const erreurCategorie = document.querySelector("label[for=categorie] span");
+
+function validerCategorie (){
+    if(selecteurCategorie.selectedIndex > 0){
+        selecteurCategorie.classList.remove("invalide");
+        erreurCategorie.classList.remove("erreur");
+        erreurCategorie.innerHTML="";
+        return true
+    }
+    else{
+        selecteurCategorie.classList.add("invalide");
+        erreurCategorie.classList.add("erreur");
+        erreurCategorie.innerHTML="Veuillez choisir une catégorie";
+    }
+}
+//Si image valide, titre valide, catégorie choisie, lance fetch
+const formAjout = document.querySelector("dialog form");
+formAjout.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // if(validerMotDePasse(inputPassword.value) && validerCategorie() ){
+    //     envoyerImage();
+    // }
+});
+
+
+
+
+
+
+// function handleFiles(files) {
+//     var imageType = /^image\//;
+//     for (var i = 0; i < files.length; i++) {
+//         var file = files[i];
+//         if (!imageType.test(file.type)) {
+//         alert("veuillez sélectionner une image");
+//         }else{
+//             if(i == 0){
+//                 preview.innerHTML = '';
+//             }
+//             var img = document.createElement("img");
+//             img.classList.add("obj");
+//             img.file = file;
+//             preview.appendChild(img); 
+//             var reader = new FileReader();
+//             reader.onload = ( function(aImg) {
+//                 return function(e) { 
+//                     aImg.src = e.target.result; 
+//                     }; 
+//             })(img);
+    
+//             reader.readAsDataURL(file);
+//         }
+//     }
+//  }
